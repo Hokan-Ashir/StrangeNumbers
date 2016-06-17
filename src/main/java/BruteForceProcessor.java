d56fabc19b793d4fb5b0535c31166865d81fd62e
Hokan-Ashir <achkasov.anton.92@gmail.com>
2016-06-17 17:59:01 +0300

    correct result based on all digit numbers

--- src/main/java/BruteForceProcessor.java
+++ src/main/java/BruteForceProcessor.java
@@ -9,16 +9,27 @@ public class BruteForceProcessor implements StrangeNumberProcessor {
         for(long i = lowBorder; i < highBorder; ++i) {
             boolean suitable = true;
             for (int j = 1; j <= highNumberNumbersBorder; ++j) {
-                int part = (int)(i / Math.pow(10.0, (int)Math.log10(i) - j));
+                long part = (long)(i / Math.pow(10.0, (int)Math.log10(i) - j));
                 if (part == 0 || part % (j + 1) != 0) {
                     suitable = false;
                     break;
                 }
             }
 
-            if (suitable) {
+            if (suitable /*&& filter(i)*/) {
                 System.out.println(i);
             }
         }
+    }
+
+    public boolean filter(long value) {
+        String stringValue = String.valueOf(value);
+        int checker = 0;
+        for (int i = 0; i < stringValue.length(); i++) {
+            int val = stringValue.charAt(i) - '0';
+            if ((checker & (1 << val)) > 0) return false;
+            checker |= (1 << val);
+        }
+        return true;
     }
 }
--- src/main/java/StrangeNumberProcessor.java
+++ src/main/java/StrangeNumberProcessor.java
@@ -3,4 +3,5 @@
  */
 public interface StrangeNumberProcessor {
     void run(int highNumberNumbersBorder);
+    boolean filter(long value);
 }
